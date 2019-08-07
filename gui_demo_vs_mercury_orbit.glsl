bd1464e9a97d70f70d60e2ed8c155aef9bf320cf
Bushra Ferdousi <31138464+bush1234@users.noreply.github.com>
2019-08-07 17:51:51 -0400

    Add files via upload

---
+++ gui_demo_vs_mercury_orbit.glsl
@@ -0,0 +1,70 @@
+#version 400            
+uniform mat4 PVM;
+uniform float time;
+
+uniform mat4 M;
+uniform mat4 V;
+uniform mat4 P;
+
+in vec3 pos_attrib;
+in vec2 tex_coord_attrib;
+in vec3 normal_attrib;
+
+out vec2 tex_coord;
+out vec4 position_ws;
+out vec3 nw_nn;
+out vec3 pass_xyz_position;
+//out vec3 lp_ws;
+
+void main(void)
+{
+   gl_Position = PVM * vec4(pos_attrib, 1.0); 
+   position_ws = M*vec4(pos_attrib,1.0);
+   pass_xyz_position = pos_attrib.xyz;
+
+   nw_nn = mat3(M)*normal_attrib; // normal in world space not normalized
+   
+   //vec4 texCoords = vec4(pos_attrib, 1.0);
+   tex_coord = tex_coord_attrib;
+   //tex_coord.x = atan(normal_attrib.z/normal_attrib.x) / (2.0 * 3.1415926) + 0.5;
+   //tex_coord.y = 0.5 - asin(normal_attrib.y) / 3.1415926;
+	//tex_coord.x = asin(normal_attrib.x)/3.1415926 + 0.5;
+	//tex_coord.y = asin(normal_attrib.y)/3.1415926 + 0.5;
+   //if(tex_coord.x < 0.1) tex_coord.x = 1.0;
+    //tex_coord = vec2((atan(texCoords.y, texCoords.x) / 3.1415926 + 1.0) * 0.5,
+   //                               (asin(texCoords.z) / 3.1415926 + 0.5));
+   
+
+   
+   //lp_ws = lightPosition_ws;
+   //distance = length(lightposition_ws-position_ws.xyz);
+
+	//float x = pos_attrib.x;
+	//float y = pos_attrib.y;
+
+	//float r = sqrt(x * x + y * y );
+
+	//float d = r ? asin(r) / r : 0.0;
+	//float d;
+	//if(r>0)
+		//d = asin(r)/r;
+	//else 
+		//d = 0.0;
+	//float x2 = d * x;
+	//float y2 = d * y;
+
+	
+	//float PI = 3.1415926;
+	//tex_coord.x = x2/(4.0*pi)+0.5;
+	//tex_coord.y = y2/(2*pi)+0.5;
+	//tex_coord.x = mod(x2 / (4.0 * pi) + 0.5, 1.0f);
+	//tex_coord.y = y2 / (2.0 * pi) + 0.5;
+
+	//float r = length(pos_attrib);
+    //float theta = acos(pos_attrib.z/r);
+	//float theta = atan(length(pos_attrib.xy), pos_attrib.z);
+    //float phi   = atan(pos_attrib.y/pos_attrib.x);
+ 
+    //tex_coord.x = clamp(0.5 + phi/(2.0*PI), 0.0, 1.0);
+    //tex_coord.y = clamp(theta/PI, 0.0, 1.0);
+}
---
+++ solar_system_fs.glsl
@@ -0,0 +1,91 @@
+#version 400
+
+uniform sampler2D texture;
+uniform vec4 lightPosition_ws; //light position in word space
+uniform vec4 La;
+uniform vec4 Ld;
+uniform vec4 Ls;
+uniform vec3 eye_pos;
+uniform float Shininess;
+uniform bool enable_texture; 
+
+
+uniform vec4 Ka,Kd,Ks;
+
+out vec4 fragcolor; 
+
+in vec2 tex_coord;
+in vec4 position_ws;
+in vec3 nw_nn;
+in vec3 pass_xyz_position;
+//in vec3 lp_ws;
+
+#define PI 3.141592653589793238462643383279
+
+void main(void)
+{   
+	if(pass_xyz_position.x==0.0 && pass_xyz_position.y==0 && pass_xyz_position.z==0.0) discard;
+	vec2 tc = tex_coord;
+	tc.x = atan(pass_xyz_position.x, pass_xyz_position.z) / (2 * PI)+0.5;
+	tc.y = 0.5 + atan(pass_xyz_position.y,length(pass_xyz_position.xz))/PI ; 
+	//ambient lighting
+    vec4 ambient = La * Ka; //ambient
+
+
+	vec4 lw_nn = lightPosition_ws-position_ws; //light direction in world space not normalize
+	float distance = length(lw_nn);
+
+	vec3 lw = normalize(vec3(lw_nn));
+	vec3 nw = normalize(nw_nn);
+	
+	float cos_angle = clamp(dot(nw,lw),0,1);
+	vec4 diffuse = (cos_angle*Ld*Kd)/(distance*distance); //diffuse 
+
+
+	//specular
+	vec3 vw_nn = eye_pos.xyz - position_ws.xyz;
+	vec3 vw = normalize(vw_nn);
+	vec3 rw_nn = reflect(-lw, nw);
+	vec3 rw = normalize(rw_nn);
+
+	float cosAlpha = clamp( dot( vw,rw ), 0,1 );
+
+	vec4 specular = Ks*Ls*pow(cosAlpha,Shininess)/(distance*distance);
+
+	// discard the specular highlight if the light’s behind the vertex
+	if (dot(nw,lw)<0.0)
+	{
+		specular = vec4(0.0,0.0,0.0,1.0);
+	}
+	
+	vec4 objectcolor = texture2D(texture, tc);
+	//fragcolor = vec4((ambient+diffuse+specular).rgb,1.0);
+
+	if (enable_texture == true)
+	{
+		fragcolor = vec4(((objectcolor * La) + (objectcolor * Ld * cos_angle / (distance*distance)) +  specular).rgb, 1.0);
+	}
+	else {
+
+		fragcolor = vec4((ambient+diffuse+specular).rgb,1.0);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
